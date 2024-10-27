c33cd5b54594180d66c722b095957ae17f6596bd
Travis Hinkelman <thinkelman@pm.me>
2024-10-26 21:00:48 -0700

    Improve performance of simulation functions

--- functions.R
+++ functions.R
@@ -1,6 +1,7 @@
 rolls_df = expand.grid(Dice1 = 1:6, Dice2 = 1:6) |> 
   dplyr::mutate(Roll = Dice1 + Dice2)  |> 
   dplyr::count(Roll) |> 
+  # by making Roll a factor, table (see below) reports zero for values that haven't been rolled yet
   dplyr::mutate(Roll = as.factor(Roll),
                 Prob = n/sum(n),
                 Steps = c(3, 6, 8, 11, 14, 16, 14, 11, 8, 6, 3),
@@ -11,7 +12,7 @@ roll <- function(n, replace = TRUE, rdf = rolls_df){
 }
 
 get_kitty <- function(base_value, scratches, rolls = NULL){
-  init = 4 * base_value * (4 + 3 + 2 + 1)
+  init = 4 * base_value * (4 + 3 + 2 + 1) # multiply by 4 for 4 suits in deck
   vals = NULL
   if (!is.null(rolls) & length(rolls) > 0){
     vals = sapply(rolls, function(x){
@@ -21,35 +22,37 @@ get_kitty <- function(base_value, scratches, rolls = NULL){
   cumsum(c(init, vals))
 }
 
-get_prob <- function(scratches, rolls, rdf = rolls_df){
-  out = data.frame(Roll = as.factor(2:12), Freq = 0) |> 
-    dplyr::bind_rows(table(rolls) |> 
-                       as.data.frame() |> 
-                       dplyr::rename(Roll = rolls)) |> 
-    dplyr::filter(!(Roll %in% scratches)) |> 
-    dplyr::group_by(Roll) |> 
-    dplyr::summarise(Freq = sum(Freq, na.rm = TRUE)) |> 
-    dplyr::left_join(dplyr::select(rdf, Roll, Prob, Steps),
-                     by = dplyr::join_by(Roll)) |> 
-    dplyr::rename(Horse = Roll) |> 
-    dplyr::mutate(StepsRemain = Steps - Freq,
-                  Prob = Prob^StepsRemain,
-                  WinProb = Prob/sum(Prob)) |> 
-    dplyr::arrange(dplyr::desc(WinProb))
-  if (out$StepsRemain[1] == 0) out$WinProb = c(1, rep(0, nrow(out) - 1))
-  out
+calc_win_prob <- function(roll_prob, steps_remain, scratches){
+  probs = roll_prob^steps_remain
+  # horse numbers range from 2:12 so need to sutract one to get index
+  probs[as.numeric(scratches) - 1] = 0
+  probs_scaled = probs/sum(probs)
+  if (min(steps_remain) < 1) probs_scaled = ifelse(probs_scaled == max(probs_scaled), 1, 0)
+  probs_scaled
+}
+
+get_win_prob <- function(scratches, rolls = NULL, rdf = rolls_df){
+  if (!is.null(rolls) & length(rolls) > 0){
+    counts = unclass(unname(table(rolls)))
+    steps_remain = rdf$Steps - counts
+  } else {
+    steps_remain = rdf$Steps
+  }
+  data.frame(Horse = as.factor(2:12),
+             StepsRemain = steps_remain,
+             WinProb = calc_win_prob(rdf$Prob, steps_remain, scratches))
 }
 
 sim_game <- function(base_value){
   scratches = roll(4, replace = FALSE)
   rolls = roll(1)
-  game = get_prob(scratches, rolls)
-  while(min(game$StepsRemain) > 0){
+  wp = get_win_prob(scratches, rolls)
+  while(min(wp$StepsRemain) > 0){
     rolls = c(rolls, roll(1))
-    game = get_prob(scratches, rolls)
+    wp = get_win_prob(scratches, rolls)
   }
   data.frame(Rolls = length(rolls),
              Kitty = max(get_kitty(base_value, scratches, rolls)), 
-             Winner = game$Horse[game$StepsRemain == 0])
+             Winner = wp$Horse[wp$StepsRemain == 0])
 } 
 
--- simulation.R
+++ simulation.R
@@ -1,21 +1,37 @@
+library(progress)
 library(dplyr)
 library(ggplot2)
 
 source("functions.R")
 
-# code is not optimized
-# 1000 reps will take many minutes
-reps = 1000  
+reps = 10000  
 base_value = 0.25
 
 sim_list = list()
+pb <- progress_bar$new(total = reps)
 for (i in 1:reps){
-  cat("\rRep", i, "of", reps)
+  pb$tick()
   sim_list[[i]] = sim_game(base_value)
 }
-sim_df = bind_rows(sim_list, .id = "Sim")
-saveRDS(sim_df, "sim_df.rds")
+sim_df = bind_rows(sim_list, .id = "Sim") |> 
+  mutate(Winner = factor(Winner, levels = c(2, 12, 3, 11, 4, 10, 5, 9, 6, 8, 7)))
 
-ggplot(sim_df, aes(x = Kitty)) +
-  geom_histogram(alpha = 0.6, color = "black", binwidth = 2) +
-  facet_wrap(~ Winner)
+sim_df |> 
+  count(Winner) |> 
+  mutate(Percent = round(n/sum(n) * 100))
+
+kitty_summ = sim_df |> 
+  group_by(Winner) |> 
+  summarise(Mean = mean(Kitty))
+  
+ggplot(left_join(sim_df, winners)) +
+  geom_histogram(aes(x = Kitty), fill = "grey90", color = "black", binwidth = 2) +
+  geom_vline(xintercept = mean(sim_df$Kitty), color = "grey50") +
+  geom_vline(data = kitty_summ, aes(xintercept = Mean), color = "grey50", linetype = "dashed") +
+  labs(x = "Kitty", y = "Number of Games") +
+  scale_x_continuous(breaks = seq(10, 50, 4)) +
+  facet_wrap(~ Winner, ncol = 2) +
+  theme_minimal() +
+  theme(panel.grid.major.x = element_blank(),
+        panel.grid.minor.x = element_blank(),
+        panel.grid.minor.y = element_blank())
