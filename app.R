57a4ef9ca3a58591e3132a85e062bd569bd7c81f
Travis Hinkelman <thinkelman@pm.me>
2026-03-28 22:24:50 -0700

    Apply air formatting

--- app.R
+++ app.R
@@ -6,25 +6,35 @@ library(plotly)
 
 source("functions.R")
 
-horse_colors = c("2" = "#a6cee3",
-                 "12" = "#1f78b4",
-                 "3" = "#b2df8a",
-                 "11" = "#33a02c",
-                 "4" = "#fb9a99",
-                 "10" = "#e31a1c",
-                 "5" = "#fdbf6f",
-                 "9" = "#ff7f00",
-                 "6" = "#cab2d6",
-                 "8" = "#6a3d9a",
-                 "7" = "#b15928")
+horse_colors = c(
+  "2" = "#a6cee3",
+  "12" = "#1f78b4",
+  "3" = "#b2df8a",
+  "11" = "#33a02c",
+  "4" = "#fb9a99",
+  "10" = "#e31a1c",
+  "5" = "#fdbf6f",
+  "9" = "#ff7f00",
+  "6" = "#cab2d6",
+  "8" = "#6a3d9a",
+  "7" = "#b15928"
+)
 
 ui <- page_fluid(
   title = "Horse Game",
   br(),
   card(
     card_header("Base Value"),
-    sliderInput(inputId = "base_value", label = "", min = 0.05, max = 1, 
-                step = 0.05, value = 0.25, pre = "$", width = "100%")
+    sliderInput(
+      inputId = "base_value",
+      label = "",
+      min = 0.05,
+      max = 1,
+      step = 0.05,
+      value = 0.25,
+      pre = "$",
+      width = "100%"
+    )
   ),
   card(
     card_header("Scratches"),
@@ -40,15 +50,15 @@ ui <- page_fluid(
     card_header("Click Button for Each Roll"),
     layout_column_wrap(
       width = "80px",
-      actionButton("2", "2"), 
-      actionButton("3", "3"), 
+      actionButton("2", "2"),
+      actionButton("3", "3"),
       actionButton("4", "4"),
       actionButton("5", "5"),
-      actionButton("6", "6"), 
-      actionButton("7", "7"), 
+      actionButton("6", "6"),
+      actionButton("7", "7"),
       actionButton("8", "8"),
       actionButton("9", "9"),
-      actionButton("10", "10"), 
+      actionButton("10", "10"),
       actionButton("11", "11"),
       actionButton("12", "12")
     )
@@ -58,71 +68,100 @@ ui <- page_fluid(
     plotlyOutput("probs_plot")
   ),
   card(
-    card_header("Kitty"), 
+    card_header("Kitty"),
     plotlyOutput("kitty_plot")
   )
 )
 
 server <- function(session, input, output) {
   rv <- reactiveValues(rolls = c())
-  
-  observeEvent(input[["2"]],{ rv$rolls = c(rv$rolls, 2) })
-  observeEvent(input[["3"]],{ rv$rolls = c(rv$rolls, 3) })
-  observeEvent(input[["4"]],{ rv$rolls = c(rv$rolls, 4) })
-  observeEvent(input[["5"]],{ rv$rolls = c(rv$rolls, 5) })
-  observeEvent(input[["6"]],{ rv$rolls = c(rv$rolls, 6) })
-  observeEvent(input[["7"]],{ rv$rolls = c(rv$rolls, 7) })
-  observeEvent(input[["8"]],{ rv$rolls = c(rv$rolls, 8) })
-  observeEvent(input[["9"]],{ rv$rolls = c(rv$rolls, 9) })
-  observeEvent(input[["10"]],{ rv$rolls = c(rv$rolls, 10) })
-  observeEvent(input[["11"]],{ rv$rolls = c(rv$rolls, 11) })
-  observeEvent(input[["12"]],{ rv$rolls = c(rv$rolls, 12) })
-  
+
+  observeEvent(input[["2"]], {
+    rv$rolls = c(rv$rolls, 2)
+  })
+  observeEvent(input[["3"]], {
+    rv$rolls = c(rv$rolls, 3)
+  })
+  observeEvent(input[["4"]], {
+    rv$rolls = c(rv$rolls, 4)
+  })
+  observeEvent(input[["5"]], {
+    rv$rolls = c(rv$rolls, 5)
+  })
+  observeEvent(input[["6"]], {
+    rv$rolls = c(rv$rolls, 6)
+  })
+  observeEvent(input[["7"]], {
+    rv$rolls = c(rv$rolls, 7)
+  })
+  observeEvent(input[["8"]], {
+    rv$rolls = c(rv$rolls, 8)
+  })
+  observeEvent(input[["9"]], {
+    rv$rolls = c(rv$rolls, 9)
+  })
+  observeEvent(input[["10"]], {
+    rv$rolls = c(rv$rolls, 10)
+  })
+  observeEvent(input[["11"]], {
+    rv$rolls = c(rv$rolls, 11)
+  })
+  observeEvent(input[["12"]], {
+    rv$rolls = c(rv$rolls, 12)
+  })
+
   scratches <- reactive({
     as.numeric(c(input$x1, input$x2, input$x3, input$x4))
   })
-  
+
   rolls <- reactive({
     factor(rv$rolls, levels = 2:12)
   })
-  
+
   steps <- reactive({
     0:length(rv$rolls)
   })
-  
+
   probs <- reactive({
-    lapply(steps(), function(n){
+    lapply(steps(), function(n) {
       rls = if (n == 0) NULL else rolls()[1:n]
       get_win_prob(scratches(), rls)
-    }) |> 
-      bind_rows(.id = "roll") |> 
+    }) |>
+      bind_rows(.id = "roll") |>
       mutate(roll = as.numeric(roll) - 1)
   })
-  
+
   output$probs_plot <- renderPlotly({
-    validate(need(length(unique(scratches())) == 4, "Scratch values must all be unique"))
-    validate(need(all(scratches() %in% 2:12), "Scratch values must be integers from 2 to 12"))
-    
-    p = ggplot(filter(probs(), !(horse %in% scratches())), 
-               aes(x = roll, y = win_prob, col = horse)) +
-      geom_point() + 
+    validate(need(
+      length(unique(scratches())) == 4,
+      "Scratch values must all be unique"
+    ))
+    validate(need(
+      all(scratches() %in% 2:12),
+      "Scratch values must be integers from 2 to 12"
+    ))
+
+    p = ggplot(
+      filter(probs(), !(horse %in% scratches())),
+      aes(x = roll, y = win_prob, col = horse)
+    ) +
+      geom_point() +
       geom_line() +
       scale_color_manual(values = horse_colors) +
       labs(x = "Roll", y = "Win Probability", color = "Horse") +
       theme_classic()
-    
+
     ggplotly(p)
   })
-  
+
   kitty <- reactive({
     get_kitty(input$base_value, scratches(), rolls())
   })
-  
+
   output$kitty_plot <- renderPlotly({
     validate(need(length(unique(scratches())) == 4, ""))
     validate(need(all(scratches() %in% 2:12), ""))
-    df = data.frame(roll = steps(),
-                    kitty = kitty())
+    df = data.frame(roll = steps(), kitty = kitty())
     p = ggplot(df, aes(x = roll, y = kitty)) +
       geom_point() +
       geom_line() +
--- functions.R
+++ functions.R
@@ -1,58 +1,66 @@
-rolls_df = expand.grid(dice1 = 1:6, dice2 = 1:6) |> 
-  dplyr::mutate(roll = dice1 + dice2)  |> 
-  dplyr::count(roll) |> 
+rolls_df = expand.grid(dice1 = 1:6, dice2 = 1:6) |>
+  dplyr::mutate(roll = dice1 + dice2) |>
+  dplyr::count(roll) |>
   # by making roll a factor, table (see below) reports zero for values that haven't been rolled yet
-  dplyr::mutate(roll = as.factor(roll),
-                steps = c(3, 6, 8, 11, 14, 16, 14, 11, 8, 6, 3),
-                prob = n/sum(n),
-                prob_steps = steps/sum(steps))
+  dplyr::mutate(
+    roll = as.factor(roll),
+    steps = c(3, 6, 8, 11, 14, 16, 14, 11, 8, 6, 3),
+    prob = n / sum(n),
+    prob_steps = steps / sum(steps)
+  )
 
-roll <- function(n, replace = TRUE, rdf = rolls_df){
+roll <- function(n, replace = TRUE, rdf = rolls_df) {
   sample(rdf$roll, size = n, replace = replace, prob = rdf$prob)
 }
 
-get_kitty <- function(base_value, scratches, rolls = NULL){
+get_kitty <- function(base_value, scratches, rolls = NULL) {
   init = 4 * base_value * (4 + 3 + 2 + 1) # multiply by 4 for 4 suits in deck
   vals = NULL
-  if (!is.null(rolls) & length(rolls) > 0){
-    vals = sapply(rolls, function(x){
+  if (!is.null(rolls) & length(rolls) > 0) {
+    vals = sapply(rolls, function(x) {
       ind = which(scratches == x)
-      val = if (length(ind) > 0) ind * base_value else 0})
+      val = if (length(ind) > 0) ind * base_value else 0
+    })
   }
   cumsum(c(init, vals))
 }
 
-get_win_prob <- function(scratches, rolls = NULL, rdf = rolls_df){
-  if (!is.null(rolls) & length(rolls) > 0){
+get_win_prob <- function(scratches, rolls = NULL, rdf = rolls_df) {
+  if (!is.null(rolls) & length(rolls) > 0) {
     counts = unclass(unname(table(rolls)))
     steps_remain = rdf$steps - counts
   } else {
     steps_remain = rdf$steps
   }
-  data.frame(horse = as.factor(2:12),
-             steps_remain = steps_remain,
-             win_prob = calc_win_prob(rdf$prob, steps_remain, scratches))
+  data.frame(
+    horse = as.factor(2:12),
+    steps_remain = steps_remain,
+    win_prob = calc_win_prob(rdf$prob, steps_remain, scratches)
+  )
 }
 
-calc_win_prob <- function(roll_prob, steps_remain, scratches){
+calc_win_prob <- function(roll_prob, steps_remain, scratches) {
   probs = roll_prob^steps_remain
   # horse numbers range from 2:12 so need to sutract one to get index
   probs[as.numeric(scratches) - 1] = 0
-  probs_scaled = probs/sum(probs)
-  if (min(steps_remain) < 1) probs_scaled = ifelse(probs_scaled == max(probs_scaled), 1, 0)
+  probs_scaled = probs / sum(probs)
+  if (min(steps_remain) < 1) {
+    probs_scaled = ifelse(probs_scaled == max(probs_scaled), 1, 0)
+  }
   probs_scaled
 }
 
-sim_game <- function(base_value){
+sim_game <- function(base_value) {
   scratches = roll(4, replace = FALSE)
   rolls = roll(1)
   wp = get_win_prob(scratches, rolls)
-  while(min(wp$steps_remain) > 0){
+  while (min(wp$steps_remain) > 0) {
     rolls = c(rolls, roll(1))
     wp = get_win_prob(scratches, rolls)
   }
-  data.frame(rolls = length(rolls),
-             kitty = max(get_kitty(base_value, scratches, rolls)), 
-             winner = wp$horse[wp$steps_remain == 0])
-} 
-
+  data.frame(
+    rolls = length(rolls),
+    kitty = max(get_kitty(base_value, scratches, rolls)),
+    winner = wp$horse[wp$steps_remain == 0]
+  )
+}
